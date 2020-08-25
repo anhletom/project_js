ff01d748b101513745e3c77bc0cbf9ce7993cfe5
Anh Le <maianh_tom@yahoo.com.vn>
2020-08-26 00:37:11 +0300

    commit to master

--- scripts.js
+++ scripts.js
@@ -3,13 +3,15 @@ function startScript() {
 }
 
 document.addEventListener("DOMContentLoaded", startScript);
-document.body.style.backgroundImage = 'url("b.jpg")';
+document.body.style.backgroundImage = 'url("bg.jpg")';
 var div = document.createElement("div");
 div.id = "title-wrapper";
 document.body.appendChild(div);
 document.getElementById("title-wrapper").style.textAlign = "center";
 document.getElementById("title-wrapper").style.top = "50%";
 document.getElementById("title-wrapper").style.fontFamily = "impact";
+document.getElementById("title-wrapper").style.textTransform = "uppercase";
+document.getElementById("title-wrapper").style.position = "absolute";
 
 var h1 = document.createElement("h1");
 h1.innerText = "Work under progress...";
