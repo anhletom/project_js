d749a086069f49b925c97e06d2ed08db7d85ae0e
Anh Le <maianh_tom@yahoo.com.vn>
2020-09-06 18:22:45 +0300

    Add bonus assignment

--- scripts.js
+++ scripts.js
@@ -21,24 +21,21 @@ function startScript() {
   document.getElementById("title").style.margin = "auto";
   document.getElementById("title").style.background = "black";
   document.getElementById("title").style.color = "white";
+
+  var navBar = document.createElement("ul");
+  navBar.innerHTML =
+    '<li><a id="home" class="btn" href="default.asp">Home</a></li><li><a id="news" class="btn" href="news.asp">News</a></li> <li><a id="contact" class="btn" href="contact.asp">Contact</a></li> <li><a id="about" class="btn">About</a></li>';
+  document.body.appendChild(navBar);
+
+  var contentEl = document.createElement("div");
+  contentEl.id = "content";
+  document.body.appendChild(contentEl);
+
+  function loadAbout() {
+    document.getElementById("content").innerText = "Welcome";
+  }
+
+  document.getElementById("about").addEventListener("click", loadAbout);
 }
 
 document.addEventListener("DOMContentLoaded", startScript);
-document.body.style.backgroundImage = 'url("bg.jpg")';
-var div = document.createElement("div");
-div.id = "title-wrapper";
-document.body.appendChild(div);
-document.getElementById("title-wrapper").style.textAlign = "center";
-document.getElementById("title-wrapper").style.top = "50%";
-document.getElementById("title-wrapper").style.fontFamily = "impact";
-document.getElementById("title-wrapper").style.textTransform = "uppercase";
-document.getElementById("title-wrapper").style.position = "absolute";
-
-var h1 = document.createElement("h1");
-h1.innerText = "Work under progress...";
-h1.id = "title";
-document.body.appendChild(title);
-document.getElementById("title").style.width = "fit-content";
-document.getElementById("title").style.margin = "auto";
-document.getElementById("title").style.background = "black";
-document.getElementById("title").style.color = "white";
